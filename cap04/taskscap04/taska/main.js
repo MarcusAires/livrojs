2285be45f685dd2644183677712047f6f7ec41f2
Marcus Gadelha <gadelhamarcus27@gmail.com>
2024-12-01 10:56:23 -0300

    parquimetro corrigido e finalizado

--- cap04/taskscap04/taska/main.js
+++ cap04/taskscap04/taska/main.js
@@ -5,14 +5,19 @@ const form = document.querySelector('form');
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
 
-    let deposito = Number(form.indeposito.value).toFixed(2);
+    const deposito = Number(form.indeposito.value).toFixed(2);
     if (deposito != 0 && deposito >= 1 && deposito < 1.75) {
-       '' tempo.textContent = `tempo: 30 min`
-        troco.textContent = deposito -  // corrigir o acumulo de dados que está ocorrendo.
-    }if(deposito>= 1.75 && deposito < 3){
-        tempo.textContent += `60 min`
-        troco.textContent += deposito - 1.75
+       tempo.textContent = `tempo: 30 min`
+        troco.textContent = `troco R$: ${(deposito - 1).toFixed(2)}`
+    }else if(deposito >= 1.75 && deposito < 3){
+        tempo.textContent = `tempo: 60 min`
+        troco.textContent = `troco R$: ${(deposito - 1.75).toFixed(2)}`
+    }else{
+        tempo.textContent = `tempo: 120 min`
+        troco.textContent =  `troco R$: ${(deposito - 3).toFixed(2)} `
     }
-        tempo.textContent += `120 min`
-        troco.textContent +=  deposito - 3
+});
+    form.addEventListener('reset', ()=>{
+        tempo.textContent = "Tempo: ";
+        troco.textContent = "Troco R$: ";
     })
