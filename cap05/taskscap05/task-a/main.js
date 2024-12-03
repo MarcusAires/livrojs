2b16d48edbdeb253f56e1dbe025ddceb35d520b5
MarcusAires <gadelhamarcus27@gmail.com>
2024-12-02 22:46:02 -0300

    fiz a task-a, mas não funciona ainda, revisar!

--- cap05/taskscap05/task-a/main.js
+++ cap05/taskscap05/task-a/main.js
@@ -1,1 +1,19 @@
-    
+const form = document.querySelector("form");
+const resp = document.querySelector("h3")
+
+form.addEventListener("submit", (e)=>{
+   
+    e.preventDefault();
+
+    const num = Number(form.inValor.value);
+    const frutas = form.inFruta.value
+    let counter = ""
+    for(let i = 1; i<=num; i++){
+        if(i % 2 === 0){
+            counter += frutas
+        }else{
+            counter += "*"
+        }
+    }
+    resp.innerText = counter;   
+})
