82f7cdac9f1997054abd208d960c90a87b7bf62a
Marcus Gadelha <gadelhamarcus27@gmail.com>
2024-12-19 15:18:56 -0300

    inicio do exercicio 1 do cap06 - status -  não finalizado ainda

---
+++ cap06/jogoseliminatorios/main.js
@@ -0,0 +1,39 @@
+const form = document.querySelector('form');
+const outLista = document.querySelector('#outLista');
+
+//array que armazena a lista de clubes
+let clubes = [];
+
+//botão para adicionar clubes a lista
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const clube = form.inClube.value;
+    clubes.push({clube});
+    
+    // reseta o forms após a adição do clube
+    // foca novamente no campo de adição do clube
+    // dispara event de click no botão lista para lista automaticamente após a adição
+    form.reset();
+    form.inClube.focus();
+    form.btListar.dispatchEvent(new Event('click'))
+    
+})
+//botão para listagem dos clubes
+form.btListar.addEventListener("click", (e)=>{
+    e.preventDefault();
+    
+    if(clubes.length === 0){
+        alert('Adicione o nome de um clube para continuar');
+    }
+    
+    let lista = '';
+    
+    for (const oclube of clubes){
+        const {clube} = oclube //desestrutura o objeto
+        
+        lista +=  + " x "
+    }
+
+    outLista.innerText = lista
+})
